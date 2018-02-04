1f1a7b1a171a35f4d0dfa68cde145f52ff37c1ee
minhynguyen <35487354+minhynguyen@users.noreply.github.com>
2018-02-05 02:01:55 +0700

    gdf

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,8 +1,7 @@
 <style>
 	#map-canvas{
 		width: 100%;
-    /*width: 1500px;*/
-		height: 550px;
+		height: 570px;
 	}
 </style>
 
@@ -21,10 +20,15 @@
   var map = new google.maps.Map(document.getElementById('map-canvas'),{
     center:{
       lat: 10.0309641000,
-      lng: 105.7689041000
+      lng: 105.7689041000,
+      
     },
-    zoom: 15
+    zoom: 15,
+    zoomControl: false,
+    streetViewControl: false,
+    scrolwheel : true,
   });
+
 
 
 @foreach ($dsLo as $dsLo)
@@ -32,7 +36,7 @@
   var lng = {{$dsLo->lng}};
 
 
-  
+
   var marker = new google.maps.Marker({
     position:{
       lat:lat,
@@ -43,16 +47,81 @@
     icon: 'http://maps.google.com/mapfiles/ms/micons/green.png'
     
   });
+  
+  // geolocate();
+  var customMapType = new google.maps.StyledMapType([
+          {stylers: [{hue: '#00FF00'}]},
+          {
+                featureType: 'water',
+                stylers: [{color: '#0033FF'}]
+              },
+              {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#f5f1e6'}]
+              },
+              {
+                featureType: 'road.arterial',
+                elementType: 'geometry',
+                stylers: [{color: '#fdfcf8'}]
+              },
+
+        ]);
+  var customMapTypeId ='custom_style'
+        map.mapTypes.set(customMapTypeId, customMapType);
+        map.setMapTypeId(customMapTypeId);
   var infowindow = new google.maps.InfoWindow();  
         google.maps.event.addListener(marker, 'click', (function(marker) {  
            return function() {  
-               var content = '<p>{{$dsLo->title}}</p>';  
+               var content = '<p>{{$dsLo->title}}</p>{{$dsLo->lat}}' + ',' + '{{$dsLo->lng}}';  
                infowindow.setContent(content);  
                infowindow.open(map, marker);  
            }  
-         })(marker)); 
- 
+         })(marker));
+
 @endforeach
+var mylocate = new google.maps.Marker({
+    position:{
+      lat:lat,
+      lng: lng
+    },
+    map:map,
+  });
+var infowindow = new google.maps.InfoWindow();  
+        google.maps.event.addListener(mylocate, 'click', (function(mylocate) {  
+           return function() {  
+               var content = '<p>My locate: </p>' + lat + ', '+ lng;  
+               infowindow.setContent(content);  
+               infowindow.open(map, mylocate);  
+           }  
+         })(mylocate));
+geolocate();
+function GeolocationControl(){
+  var geoButton = document.getElementById('curent-location');
+  google.maps.event.addListener(geoButton, 'click', geolocate); 
+};
+function geolocate(){
+  if (navigator.geolocation) { //nếu trình duyệt lấy đc vị trí
+          navigator.geolocation.getCurrentPosition(function(position) {
+
+            // console.log(position);
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+
+            map.setCenter(pos);
+            mylocate.setPosition(pos);
+
+          });
+        }else{
+          alert('use location');
+        }
+  };
+  
+  GeolocationControl();
+  // geolocate();
 
 
 </script>
